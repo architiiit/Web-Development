613b7dffa7382e2fc7201c2b186bfccdf1c89137
architiiit <architvashist4728@gmail.com>
2024-03-04 18:12:06 +0530

    Java script class1 and 2

---
+++ WEEK5_JS/class1/index.js
@@ -0,0 +1,119 @@
+// // line below is used to print on the console window
+// console.log('Namaste Dunia Version 4');
+
+// //variable
+// let a=5;
+// console.log(a);
+
+// let b='Hello';
+// console.log(b);
+
+
+// let c='true';
+// console.log(c);
+
+// //let and var differ from each other in scope
+// //let cannot be redifined
+
+// //constants
+// const num=5;
+// // num=9;//not possible to reassign
+
+// //Dynamic typing
+// let lastName='Babbar';
+// console.log(lastName);
+// lastName=3;
+// console.log(lastName);
+
+// //Reference types->objects,arrays,Function
+// let names=['A','B','C'];
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+
+
+// let x=1;
+// let y=4;
+// let z=x+y;
+// console.log(z);
+
+// //assignment
+// let p=5;
+// p=p/2;
+// p/=2;
+// console.log(p);
+
+// //comparison
+// console.log(2>3);
+// console.log(3>=3);
+// console.log(3===3);
+// console.log(3!==3);
+// console.log(1=='1');//only data should be same->loose equality
+// console.log(1==='1');//data and value both should be same->starict equality
+
+//ternary operator
+// let age=18;
+// let st=(age>=18)?'I can vote':'Cannot vote';
+// console.log(st);
+
+
+//logical operator->and,or,not
+
+// //if else
+
+// let marks=90;
+// if(marks>=90)
+// {
+//     console.log('Grade A');
+// }
+// else if(marks>=80)
+// {
+//     console.log('Grade B');
+// }
+// else if(marks>=70)
+// {
+//     console.log('Grade C');
+
+// }
+// else
+// {
+//     console.log('Grade D');
+// }
+
+
+// //switch case
+// let num=2;
+
+// switch(num)
+// {
+//     case 1:console.log('A');
+//     break;
+//     case 2:console.log('B');
+//     break;
+//     case 3:console.log('C');
+//     break;
+//     default:console.log('D');
+
+// }
+
+// //Loops
+// for(let i=1; i<=5;i++)
+// {
+//     console.log(i);
+// }
+
+// let x=1;
+// while(x<=5)
+// {
+//     console.log(x);
+//     x++;
+// }
+
+//do while loop
+//atlleast one time it will run
+
+// let y=1;
+// do{
+//     console.log(y);
+//     y++;
+// }while(y<10)
---
+++ WEEK5_JS/class2/index.js
@@ -0,0 +1,215 @@
+console.log('Lets start')
+
+//object create
+
+// let rectangle={
+//     length:1,
+//     breadth:2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
+// let rectangle1={
+// length:1,
+//     breadth:2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
+// let rectangle2={
+//     length:1,
+//     breadth:2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
+
+
+
+// //Factory function->creates an object and returns it
+// function createRectangle(len,bre) {
+//     return rectangle={
+//         length:len,
+//         breadth:bre,
+        
+//         draw() {
+//             console.log('drawing rectangle');
+//         }
+
+//     };
+// }
+
+// //now object creation will be equal to the object inside factory function
+// let rectangleObj1=createRectangle(4,5);//output of factory function will be stored in the variable
+// let rectangle1=createRectangle(7,8);
+// let rectangle2=createRectangle(4,9);
+
+
+
+
+
+// //Camelcase ->example: numberOfStudents
+// //Constructor Function ->Pascal Notation->example:NumberOfStudent
+// //constructor function ->props/methods->define/initialise
+// function Rectangle(len,bre){
+//     this.length=len;
+//     this.breadth=bre;
+//     this.draw=function()
+//     {
+//         console.log('drawing');
+//     }
+// }
+// Rectangle.length;//since a function is also an object so we can access some features using dot operator
+
+// //object creation using constructor
+// let rectangleObject=new Rectangle(3,6);
+
+// //Dynamic addition of new feature
+// rectangleObject.color = 'yellow';
+// console.log(rectangleObject);
+
+// //Deletion of feature
+// delete rectangleObject.length;
+// console.log(rectangleObject);
+
+
+//constructor property    
+
+// //Internally kisi bhi constructor ka constructor aise implement hota hai
+// let Rectangle1=new Function(
+// 'length','breadth',
+// `this.length=length;
+// this.breadth=breadth;
+// this.draw=function()
+// {
+//     console.log('drawing');
+// }`);
+
+// //object creation using Rectangle1
+// let rect=new Rectangle1(2,3);
+// rect.length=5;//function is an object so we can access its features using dot operator
+// console.log(rect);
+
+
+//Functions are objects
+
+// //Types in JS
+// let a=10;
+// let b=a;
+// a++;
+// console.log(a);
+// console.log(b);
+
+
+// let a={
+//     value:10
+// }
+// let b=a;
+// a.value++;
+// console.log(a.value);
+// console.log(b.value);
+
+
+
+// let a=10;
+// function inc(a)
+// {//primitive pass hota hai to copy banti hai
+//     a++;
+// }
+// inc(a);
+// console.log(a);
+
+
+// let a={
+//     value:10
+// };
+// function inc(a)
+// {
+//     a.value++;
+// }
+// inc(a);
+// console.log(a.value);
+
+
+// //LOOPS
+
+// let rectangle = {
+//     length:2,
+//     breadth:4
+// };
+// // //for-in loop
+// for(let key in rectangle)
+// {   //keys are reflected through key variable 
+//     //values are reflected through rectangle[key]
+//     console.log(key,rectangle[key]);
+// }
+
+// //for-of loop
+// for(let key of Object.entries(rectangle))
+// {
+//     console.log(key);
+// }
+
+// //kisi object ke andar koi property exist krti hai ya nhi ye check kaise hofa
+// if('color' in rectangle)
+// {
+//     console.log('Present');
+// }
+// else{
+//     console.log('Absent');
+// }
+
+
+
+
+//Object cloning
+
+// //Iterartion->clone#1
+// let src={
+//     a:10,
+//     b:20,
+//     c:30
+// };
+// let dest={};
+// for(let key in src){
+//     dest[key]=src[key];
+// }
+// console.log(dest);
+// src.a++;
+// console.log(dest);
+
+// //object cloning #2
+// let src={
+//     a:10,
+//     b:20,
+//     c:30
+// };
+
+// let src2={
+//     value:25
+// };
+
+
+// let dest=Object.assign({},src,src2);//multiple objects can be copied
+// console.log(dest);
+
+// src.a++;
+// console.log(dest);
+
+//object clone #3
+let src={
+    a:10,
+    b:20,
+    c:30
+};
+let dest={...src};
+console.log(dest);
+src.a++;
+console.log(dest);
+
